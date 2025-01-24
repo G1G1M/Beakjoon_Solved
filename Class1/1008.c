5ea2194f4f6a69a1e7936dc9a0069712f45edb3b
Kim Jiwon <kjw100404@gmail.com>
2025-01-24 16:42:01 +0900

    class1 문제 풀이

---
+++ Class1/1008.c
@@ -0,0 +1,11 @@
+// A/B
+#include <stdio.h>
+
+int main()
+{
+  int a, b;
+  scanf("%d %d", &a, &b);
+  printf("%lf\n", (double)a / b); // (type)으로 type 변환 가능
+
+  return 0;
+}
---
+++ Class1/1330.c
@@ -0,0 +1,23 @@
+// 두 수 비교하기
+#include <stdio.h>
+
+int main()
+{
+  int a, b;
+  scanf("%d %d", &a, &b);
+
+  if (a > b)
+  {
+    printf(">\n");
+  }
+  else if (a < b)
+  {
+    printf("<\n");
+  }
+  else
+  {
+    printf("==\n");
+  }
+
+  return 0;
+}
